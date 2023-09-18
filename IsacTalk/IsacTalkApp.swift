610b2f384c04a9e395ba25581e3fb0ec78799ec2
신이삭 <isac9305@gmail.com>
2023-09-18 14:03:51 +0900

    Inital commit.

---
+++ IsacTalk/IsacTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsacTalkApp.swift
+//  IsacTalk
+//
+//  Created by 신이삭 on 2023/09/18.
+//
+
+import SwiftUI
+
+@main
+struct IsacTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
